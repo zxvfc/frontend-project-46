c030908796a7ed78119ea64107dad2d53240f794
zxvfc <velya.96@gmail.com>
2023-07-03 12:14:44 +0200

    added base handling of cli arguments

---
+++ gendiff.js
@@ -0,0 +1,11 @@
+const { program } = require('commander');
+
+program
+    .description('Compares two configuration files and shows a difference.')
+    .version('1.0.0')
+    .option('-f --format <type>', 'output format')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+;
+
+program.parse();
