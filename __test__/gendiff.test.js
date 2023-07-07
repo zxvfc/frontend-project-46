243ad9ae2d0925797226fbc6d3d0704163b5eca2
zxvfc <velya.96@gmail.com>
2023-07-07 11:53:56 +0200

    updated fixtures and tests

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -7,18 +7,32 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = pathUtils.dirname(__filename);
 
 const FIXTURES_FOLDER = pathUtils.resolve(__dirname, '..', '__fixtures__');
-const format = 's';
+const format = 'stylish';
 
 const buildFixturePath = (fixtureName) => pathUtils.resolve(FIXTURES_FOLDER, fixtureName);
 
+const exp1 = fs.readFileSync(buildFixturePath('stylish-12'), 'utf8');
+const exp2 = fs.readFileSync(buildFixturePath('stylish-34'), 'utf8');
+const exp3 = fs.readFileSync(buildFixturePath('stylish-empty1'), 'utf8');
+
 const jsonFile1 = buildFixturePath('file1.json');
 const jsonFile2 = buildFixturePath('file2.json');
-const exp1 = fs.readFileSync(buildFixturePath('stylish-result1'), 'utf8');
+
+const jsonFile3 = buildFixturePath('file3.json');
+const jsonFile4 = buildFixturePath('file4.json');
 
 const yamlFile1 = buildFixturePath('file1.yaml');
-const yamlFile2 = buildFixturePath('file2.yaml');
+const yamlFile2 = buildFixturePath('file2.yml');
+
+const yamlFile3 = buildFixturePath('file3.yaml');
+const yamlFile4 = buildFixturePath('file4.yml');
+
+const emptyFile = buildFixturePath('empty.json');
 
 test('genDiff', () => {
   expect(genDiff(jsonFile1, jsonFile2, format)).toBe(exp1);
   expect(genDiff(yamlFile1, yamlFile2, format)).toBe(exp1);
+//  expect(genDiff(jsonFile3, jsonFile4, format)).toBe(exp2);
+//  expect(genDiff(yamlFile3, yamlFile4, format)).toBe(exp2);
+  expect(genDiff(emptyFile, jsonFile1, format)).toBe(exp3);
 });
